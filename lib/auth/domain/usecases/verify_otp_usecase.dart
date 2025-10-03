dbf59a9c0ce41e306d19ae557380f8610484daaf
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-10-03 15:48:43 +0700

    fix: register missing parameter

--- lib/auth/domain/usecases/verify_otp_usecase.dart
+++ lib/auth/domain/usecases/verify_otp_usecase.dart
@@ -3,7 +3,7 @@ import 'package:vocabu_rex_mobile/auth/domain/repositories/auth_repository.dart'
 class VerifyOtpUsecase {
   final AuthRepository authRepository;
   VerifyOtpUsecase({required this.authRepository});
-  Future<void> call(String userId, String otp) async {
-    await authRepository.verifyOtp(userId, otp);
+  Future<Map<String, dynamic>> call(String userId, String otp) async {
+    return await authRepository.verifyOtp(userId, otp);
   }
 }
