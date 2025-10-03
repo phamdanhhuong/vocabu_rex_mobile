1e51ecd3118b2caed334bfa28c5b88f46b8e8a46
Phạm Danh Hưởng <danhhuong246@gmail.com>
2025-10-03 16:23:28 +0700

    feat: Add Option widget for selectable choices in ListenChoose component

--- lib/exercise/ui/widgets/listen_choose.dart
+++ lib/exercise/ui/widgets/listen_choose.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vocabu_rex_mobile/constants/app_colors.dart';
 import 'package:vocabu_rex_mobile/exercise/domain/entities/exercise_meta_entity.dart';
 import 'package:vocabu_rex_mobile/exercise/ui/widgets/audio_button.dart';
+import 'package:vocabu_rex_mobile/exercise/ui/widgets/option.dart';
 
 class ListenChoose extends StatefulWidget {
   final ListenChooseMetaEntity meta;
@@ -81,35 +82,5 @@ class _ListenChooseState extends State<ListenChoose> {
         }
       });
     }
-  }
-}
-
-class Option extends StatelessWidget {
-  final String label;
-  final bool isSelected;
-
-  const Option({super.key, required this.isSelected, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        height: 50.h,
-        margin: EdgeInsets.symmetric(vertical: 8.h, horizontal: 16.w),
-        decoration: BoxDecoration(
-          color: AppColors.transparent,
-          borderRadius: BorderRadius.all(Radius.circular(18.r)),
-          border: isSelected
-              ? Border.all(color: AppColors.primaryBlue, width: 3)
-              : Border.all(color: AppColors.borderGrey),
-        ),
-        child: Center(
-          child: Text(
-            label,
-            style: TextStyle(color: AppColors.characterBlue, fontSize: 16.sp),
-          ),
-        ),
-      ),
-    );
   }
 }
